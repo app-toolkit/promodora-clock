848a8b09c2cbf793cb57114451cc257677787d41
trumandu <1753177225@qq.com>
2024-03-23 22:05:18 +0800

    init

---
+++ preload.js
@@ -0,0 +1,8 @@
+/* eslint-disable func-names */
+/* eslint-disable no-undef */
+document.addEventListener('DOMContentLoaded', function () {
+  const element = document.querySelector('header>h2');
+  if (element) {
+    element.style.display = 'none';
+  }
+});
